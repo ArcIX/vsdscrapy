6eb9179f60b84c45205a97c5ea091a04c572de79
Ronel Christian Alvarez <rc2582739@gmail.com>
2021-05-12 14:28:01 +0800

    Correct xpath for scraping symbol details

--- warrantscrape/warrantscrape/warrant_spider.py
+++ warrantscrape/warrantscrape/warrant_spider.py
@@ -2,34 +2,47 @@ from scrapy import Spider
 from scrapy.http import Request
 
 class WarrantSpider(Spider):
-    base_url = "https://vsd.vn"
     name = "Warrant"
+    base_url = "https://vsd.vn"
     search_url = base_url + "/en/search?text="
 
     def set_symbols(self, symbols):
         """
-        Give the symbols of the securities to be scraped
+        Provide the symbols of the securities to be scraped
         """
         self.search = symbols
 
     def search_symbols(self):
+        """
+        On this page: https://vsd.vn/en/search?text=, make a request to search for each symbol
+        """
         for symbol in self.symbols:
             yield Request(self.search_url + symbol, callback=self.get_detail_page_url, meta={'symbol': symbol})
         else:
             raise ValueError("Symbols to search not provided using set_symbols")
 
     def get_detail_page_url(self, response):
+        """
+        Upon searching the symbol, get the url to
+        its details page
+        """
         symbol = response.meta.get('symbol')
         return response.xpath("//*[@id='divGlSearchIsuStocks']//li//b[text()='" + symbol + "']/../@href")
 
     def start_requests(self):
+        """
+        After getting a list of all urls to each symbol's details page, make a request to each of them
+        """
         detail_page_urls = self.search_symbols()
         for detail_page_url in detail_page_urls:
             yield Request(detail_page_url, dont_filter=True)
 
     def parse(self, response):
-        for row in response.xpath("//*[@id='Detail_TCPH_TTCK']/div[@class='news-issuers']/div[@class='row']"):
-            key = row.xpath("/div[1]/text()").get()
+        """
+        From the details page of a symbol, get all information for that security
+        """
+        for row in response.xpath("//div[@id='Detail_TCPH_TTCK']/div[@class='news-issuers']/div[@class='row']"):
+            key = row.xpath("./div[1]/text()").get()
             key = key.replace(":", "")
-            value = row.xpath("/div[2]//*[not(child::*)]/text()").get()
+            value = row.xpath("./div[2]/descendant-or-self::*[last()]/text()").get()
             yield {key: value}
