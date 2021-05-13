1245004233fde59d2ad848bfc61a5551ef012286
Ronel Christian Alvarez <rc2582739@gmail.com>
2021-05-13 17:16:55 +0800

    Rearrange callback sequence for symbol search Add handler for http response 302

--- warrantscrape/warrantscrape/spiders/warrant_spider.py
+++ warrantscrape/warrantscrape/spiders/warrant_spider.py
@@ -6,11 +6,26 @@ class WarrantSpider(Spider):
     base_url = "https://vsd.vn"
     search_url = base_url + "/en/search?text="
 
+    def start_requests(self):
+        self.set_symbols(
+            [
+                "CROS2001",
+                "CROS2002"
+            ]
+        )
+        for index, url in enumerate(self.start_urls):
+            yield Request(url, dont_filter=True, meta = {
+                'symbol': self.symbols[index],
+                'dont_redirect': True,
+                'handle_httpstatus_list': [301, 302],
+            }, callback=self.get_detail_page_url)
+
     def set_symbols(self, symbols):
         """
         Provide the symbols of the securities to be scraped
         """
-        self.search = symbols
+        self.symbols = symbols
+        self.start_urls = [self.search_url + symbol for symbol in self.symbols]
 
     def search_symbols(self):
         """
@@ -28,22 +43,25 @@ class WarrantSpider(Spider):
         """
         symbol = response.meta.get('symbol')
         href = response.xpath("//div[@id='divGlSearchIsuStocks']//li//b[text()='" + symbol + "']/../@href").get()
-        yield self.base_url + href
-
-    def start_requests(self):
-        """
-        After getting a list of all urls to each symbol's details page, make a request to each of them
-        """
-        detail_page_urls = self.search_symbols()
-        for detail_page_url in detail_page_urls:
-            yield Request(detail_page_url, dont_filter=True)
+        yield Request(self.base_url + href, dont_filter=True, meta = {
+                'dont_redirect': True,
+                'handle_httpstatus_list': [301, 302],
+        })
 
     def parse(self, response):
         """
         From the details page of a symbol, get all information for that security
         """
-        for row in response.xpath("//div[@id='Detail_TCPH_TTCK']/div[@class='news-issuers']/div[@class='row']"):
+        # page = response.url.split("/")[-2]
+        # filename = 'quotes-%s.html' % page
+        # with open(filename, 'wb') as f:
+        #     f.write(response.body)
+        security = {}
+        rows = response.xpath("//div[@id='Detail_TCPH_TTCK']/div[@class='news-issuers']/div[@class='row']")[:-1]
+        for row in rows:
             key = row.xpath("./div[1]/text()").get()
             key = key.replace(":", "")
             value = row.xpath("./div[2]/descendant-or-self::*[last()]/text()").get()
-            yield {key: value}
+            security[key] = value
+        return security
+            
