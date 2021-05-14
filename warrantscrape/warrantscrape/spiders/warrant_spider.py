4a3cdd97f0613b20ec893813f68f2d8d302844fb
Ronel Christian Alvarez <rc2582739@gmail.com>
2021-05-14 15:20:24 +0800

    Integrate ItemLoader when parsing response

--- warrantscrape/warrantscrape/spiders/warrant_spider.py
+++ warrantscrape/warrantscrape/spiders/warrant_spider.py
@@ -1,40 +1,38 @@
-from scrapy import Spider
+from scrapy import Spider, Field
 from scrapy.http import Request
+from scrapy.loader import ItemLoader
+from ..items import WarrantItem
+from scrapy.loader.processors import TakeFirst
 
 class WarrantSpider(Spider):
     name = "warrant"
     base_url = "https://vsd.vn"
     search_url = base_url + "/en/search?text="
 
-    def start_requests(self):
-        self.set_symbols(
-            [
-                "CROS2001",
-                "CROS2002"
-            ]
-        )
-        for index, url in enumerate(self.start_urls):
-            yield Request(url, dont_filter=True, meta = {
-                'symbol': self.symbols[index],
-                'dont_redirect': True,
-                'handle_httpstatus_list': [301, 302],
-            }, callback=self.get_detail_page_url)
-
     def set_symbols(self, symbols):
         """
         Provide the symbols of the securities to be scraped
         """
         self.symbols = symbols
-        self.start_urls = [self.search_url + symbol for symbol in self.symbols]
 
-    def search_symbols(self):
+    def start_requests(self):
         """
         On this page: https://vsd.vn/en/search?text=, make a request to search for each symbol
         """
-        for symbol in self.symbols:
-            yield Request(self.search_url + symbol, callback=self.get_detail_page_url, meta={'symbol': symbol})
-        else:
-            raise AttributeError("Symbols to search not provided using set_symbols")
+        self.set_symbols([
+            "CROS2001",
+            "CROS2002"
+        ])
+        search_urls = [self.search_url + symbol for symbol in self.symbols]
+        for index, url in enumerate(search_urls):
+            yield Request(url, dont_filter=True, 
+                meta={
+                    'symbol': self.symbols[index],
+                    'dont_redirect': True,
+                    'handle_httpstatus_list': [301, 302],
+                },
+                callback=self.get_detail_page_url
+            )
 
     def get_detail_page_url(self, response):
         """
@@ -42,26 +40,35 @@ class WarrantSpider(Spider):
         its details page
         """
         symbol = response.meta.get('symbol')
-        href = response.xpath("//div[@id='divGlSearchIsuStocks']//li//b[text()='" + symbol + "']/../@href").get()
-        yield Request(self.base_url + href, dont_filter=True, meta = {
+        href = response.xpath(
+            """
+            //div[@id='divGlSearchIsuStocks']//li//b[text()='{}']/../@href
+            """.format(symbol)
+        ).get()
+        yield Request(self.base_url + href, dont_filter=True,
+            meta = {
                 'dont_redirect': True,
-                'handle_httpstatus_list': [301, 302],
-        })
+                'handle_httpstatus_list': [301, 302]
+            }
+        )
 
     def parse(self, response):
         """
         From the details page of a symbol, get all information for that security
         """
-        # page = response.url.split("/")[-2]
-        # filename = 'quotes-%s.html' % page
-        # with open(filename, 'wb') as f:
-        #     f.write(response.body)
-        security = {}
-        rows = response.xpath("//div[@id='Detail_TCPH_TTCK']/div[@class='news-issuers']/div[@class='row']")[:-1]
+        rows = response.xpath(
+            """
+            //div[@id='Detail_TCPH_TTCK']/div[@class='news-issuers']/div[@class='row']
+            """
+        )[:-1]
+        warrantItem = WarrantItem()
+        warrantLoader = ItemLoader(item=warrantItem, response=response)
         for row in rows:
             key = row.xpath("./div[1]/text()").get()
             key = key.replace(":", "")
+            warrantItem.fields[key] = Field(output_processor=TakeFirst())
+
             value = row.xpath("./div[2]/descendant-or-self::*[last()]/text()").get()
-            security[key] = value
-        return security
+            warrantLoader.add_value(key, value)
+        return warrantLoader.load_item()
             
