69a99c8a540bbf52c3ccb4016b980d10f2d36a82
ArcIX <rc2582739@gmail.com>
2021-05-22 12:20:41 +0800

    Add load symbols to scrape from csv file feature

--- warrantscrape/warrantscrape/spiders/warrant_spider.py
+++ warrantscrape/warrantscrape/spiders/warrant_spider.py
@@ -3,6 +3,8 @@ from scrapy.http import Request
 from scrapy.loader import ItemLoader
 from ..items import WarrantItem
 from scrapy.loader.processors import TakeFirst
+import os
+import csv
 
 class WarrantSpider(Spider):
     name = "warrant"
@@ -11,7 +13,8 @@ class WarrantSpider(Spider):
 
     def set_symbols(self, symbols):
         """
-        Provide the symbols of the securities to be scraped
+        Provide the symbols of the securities to be scraped.
+        DO NOT USE THIS IF SYMBOLS WILL BE PROVIDED VIA CSV
         """
         self.symbols = symbols
 
@@ -24,6 +27,14 @@ class WarrantSpider(Spider):
         #     "CROS2001",
         #     "CROS2002"
         # ])
+        input_file = os.path.join(os.path.dirname(__file__), "../../../input/inputsymbols.csv")
+        if not hasattr(self, 'symbols') and os.path.exists(input_file):
+            with open(input_file, newline="") as f:
+                reader = csv.reader(f)
+                self.symbols = [row[0] for row in reader]
+                print(self.symbols)
+        else:
+            raise AttributeError("No symbols provided.")
         search_urls = [self.search_url + symbol for symbol in self.symbols]
         for index, url in enumerate(search_urls):
             yield Request(url, dont_filter=True, 
@@ -38,7 +49,7 @@ class WarrantSpider(Spider):
     def get_detail_page_url(self, response):
         """
         Upon searching the symbol, get the url to
-        its details page
+        its details page and make a request to view it
         """
         symbol = response.meta.get('symbol')
         href = response.xpath(
@@ -71,5 +82,6 @@ class WarrantSpider(Spider):
 
             value = row.xpath("./div[2]/descendant-or-self::*[last()]/text()").get()
             warrantLoader.add_value(key, value)
+        warrantItem.fields['Source URL'] = Field(output_processor=TakeFirst())
+        warrantLoader.add_value('source_url', response.request.url)
         return warrantLoader.load_item()
-            
