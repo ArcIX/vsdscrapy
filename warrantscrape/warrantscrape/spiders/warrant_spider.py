4a72065887671f9011b7976464aaa4a875d6c033
Ronel Christian Alvarez <rc2582739@gmail.com>
2021-05-27 19:33:35 +0800

    Correct dictionary iteration Load response to loader Change xpaths to any descendant

--- warrantscrape/warrantscrape/spiders/warrant_spider.py
+++ warrantscrape/warrantscrape/spiders/warrant_spider.py
@@ -20,17 +20,11 @@ class WarrantSpider(Spider):
         """
         On this page: https://vsd.vn/en/search?text=, make a request to search for each symbol
         """
-        # Sample symbols to scrape
-        # self.set_symbols([
-        #     "CROS2001",
-        #     "CROS2002"
-        # ])
         input_file = os.path.join(os.path.dirname(__file__), "../../../input/inputsymbols.csv")
         if not hasattr(self, 'symbols') and os.path.exists(input_file):
             with open(input_file, newline="") as f:
                 reader = csv.reader(f)
                 self.symbols = [row[0] for row in reader]
-                print(self.symbols)
         else:
             raise AttributeError("No symbols provided.")
         search_urls = [self.search_url + symbol for symbol in self.symbols]
@@ -68,35 +62,31 @@ class WarrantSpider(Spider):
         """
         From the details page of a symbol, get all information for that security
         """
-        warrantLoader = WarrantLoader(
-            selector = """
-            //div[@id='Detail_TCPH_TTCK']/div[@class='news-issuers']
-            """
-        )
+        warrantLoader = WarrantLoader(response=response)
         xpaths = {
-            'issuer' : "./div[text()=\"Issuer's name:\"]/following-sibling::node()/a/text()",
-            'name' : "./div[text()='Name of warrant:']/following-sibling::node()/text()",
-            'code' : "./div[text()='Warrant code:']/following-sibling::node()/text()",
-            'isin' : "./div[text()='ISINs:']/following-sibling::node()/text()",
-            'underlying' : "./div[text()='Underlying securities:']/following-sibling::node()/text()",
-            'ul_issuer' : "./div[text()='Issuer of underlying securities:']/following-sibling::node()/text()",
-            'wrt_type' : "./div[text()='Warrant type:']/following-sibling::node()/text()",
-            'exer_style' : "./div[text()='Exercise style:']/following-sibling::node()/text()",
-            'settle_method' : "./div[text()='Settlement method:']/following-sibling::node()/text()",
-            'term' : "./div[text()='Term:']/following-sibling::node()/text()",
-            'due_date' : "./div[text()='Due date:']/following-sibling::node()/text()",
-            'conv_ratio' : "./div[text()='Conversion ratio:']/following-sibling::node()/text()",
-            'strike_price' : "./div[text()='Strike price:']/following-sibling::node()/text()",
-            'issue_price' : "./div[text()='Initial issuance price:']/following-sibling::node()/text()",
-            'market' : "./div[text()='Trading market:']/following-sibling::node()/text()",
-            'no_of_reg_cert' : "./div[text()='Number of Covered warrant Registration Certificate:']/following-sibling::node()/text()",
-            'no_of_init_reg' : "./div[text()='Number of initially registered covered warrants at VSD:']/following-sibling::node()/text()",
-            'val_of_init_reg' : "./div[text()='The value of initially registered covered warrant basing on the issuance price at VSD:']/following-sibling::node()/text()",
-            'qty_of_init_reg' : "./div[text()='Quantity of registered covered warrants at VSD:']/following-sibling::node()/text()",
-            'issuer_method' : "./div[text()='Issuer method:']/following-sibling::node()/text()",
-            'admin_place' : "./div[text()='Administration Place:']/following-sibling::node()/text()"
+            'issuer' : "//div[text()=\"Issuer's name:\"]/following-sibling::node()/a/text()",
+            'name' : "//div[text()='Name of warrant:']/following-sibling::node()/text()",
+            'code' : "//div[text()='Warrant code:']/following-sibling::node()/text()",
+            'isin' : "//div[text()='ISINs:']/following-sibling::node()/text()",
+            'underlying' : "//div[text()='Underlying securities:']/following-sibling::node()/text()",
+            'ul_issuer' : "//div[text()='Issuer of underlying securities:']/following-sibling::node()/text()",
+            'wrt_type' : "//div[text()='Covered warrant type:']/following-sibling::node()/text()",
+            'exer_style' : "//div[text()='Excercise style:']/following-sibling::node()/text()",
+            'settle_method' : "//div[text()='Settlement method:']/following-sibling::node()/text()",
+            'term' : "//div[text()='Term:']/following-sibling::node()/text()",
+            'due_date' : "//div[text()='Due date:']/following-sibling::node()/text()",
+            'conv_ratio' : "//div[text()='Conversion ratio:']/following-sibling::node()/text()",
+            'strike_price' : "//div[text()='Strike price:']/following-sibling::node()/text()",
+            'issue_price' : "//div[text()='Initial issuance price:']/following-sibling::node()/text()",
+            'market' : "//div[text()='Trading market:']/following-sibling::node()/text()",
+            'no_of_reg_cert' : "//div[text()='Number of Covered warrant Registration Certificate:']/following-sibling::node()/text()",
+            'no_of_init_reg' : "//div[text()='Number of initially registered covered warrants at VSD:']/following-sibling::node()/text()",
+            'val_of_init_reg' : "//div[text()='The value of initially registered covered warrant basing on the issuance price at VSD:']/following-sibling::node()/text()",
+            'qty_of_init_reg' : "//div[text()='Quantity of registered covered warrants at VSD:']/following-sibling::node()/text()",
+            'issuer_method' : "//div[text()='Issuer method:']/following-sibling::node()/text()",
+            'admin_place' : "//div[text()='Administration Place:']/following-sibling::node()/text()"
         }
-        for key, value in xpaths:
-            warrantLoader.add_xpath(key, value)
+        for key in xpaths:
+            warrantLoader.add_xpath(key, xpaths[key])
         warrantLoader.add_value('source_url', response.request.url)
         return warrantLoader.load_item()
